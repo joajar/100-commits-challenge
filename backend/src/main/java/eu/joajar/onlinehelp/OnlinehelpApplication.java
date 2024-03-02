4c0648808ad481ab44739c3d70ec896042404b74
joajar <joanna.z.jaroszewska@gmail.com>
2024-03-02 12:25:46 +0100

    Initial commit for Spring backend part of the project

---
+++ backend/src/main/java/eu/joajar/onlinehelp/OnlinehelpApplication.java
@@ -0,0 +1,13 @@
+package eu.joajar.onlinehelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinehelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinehelpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/eu/joajar/onlinehelp/OnlinehelpApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.joajar.onlinehelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinehelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
