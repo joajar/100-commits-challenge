13919b6bcccd429aca8dbb750e9b36d6f6108edb
joajar <joanna.z.jaroszewska@gmail.com>
2024-03-11 22:04:16 +0100

    FE feat: added sketch of contact form template

--- frontend/src/app/content/home/home.component.ts
+++ frontend/src/app/content/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../../model/message';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent {  
 
+  message: Message = {name: '', email: '', content: ''};
+
+  send() {
+    console.log(this.message);
+  }
 }
