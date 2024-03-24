40e33044ee002165c42dde73eb2caaafa06b1194
joajar <joanna.z.jaroszewska@gmail.com>
2024-03-24 14:53:07 +0100

    FE feat: developed displaying contact data at the footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
