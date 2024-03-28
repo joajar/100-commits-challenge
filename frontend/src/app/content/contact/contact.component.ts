8e296012b62fb2ac63ead83e45d0409b87a363c7
joajar <joanna.z.jaroszewska@gmail.com>
2024-03-28 23:06:04 +0100

    FE feat: started developing contact component

--- frontend/src/app/content/contact/contact.component.ts
+++ frontend/src/app/content/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
