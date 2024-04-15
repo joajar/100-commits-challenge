320c6085b1e2ac96cd7d70f8bff83370fc819eb2
joajar <joanna.z.jaroszewska@gmail.com>
2024-04-15 22:54:47 +0200

    FE feat: developed button at the home component

--- frontend/src/app/content/home/home.component.ts
+++ frontend/src/app/content/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Message } from '../../model/message';
 
@@ -8,6 +9,7 @@ import { Message } from '../../model/message';
   standalone: true,
   imports: [
     FormsModule,
+    MatButtonModule,
     MatIconModule
   ],
   templateUrl: './home.component.html',
